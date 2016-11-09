e1e08e09c35168bf84185603766441c3b12eafae
mconnors333 <matthew.connors333@gmail.com>
2016-11-09 16:01:35 -0500

    Super amazing javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,26 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+
+    alert("You found a match!");
+
+} else {
+    alert("Sorry, try again.")
+}
+if (cardTwo === cardOne) {
+
+    alert("You found a match!");
+
+} else {
+    alert("Sorry, try again.")
+}
+
+if (cardThree === cardFour) {
+
+    alert("You found a match!");
+
+} else {
+    alert("Sorry, try again.")
+}
