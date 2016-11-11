eda081797f2641aa103394c9bf247106edefeef3
mconnors333 <matthew.connors333@gmail.com>
2016-11-11 10:45:19 -0500

    added JS and CSS

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,20 +3,20 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-
+/*
 if (cardTwo === cardFour) {
 
     alert("You found a match!");
 
 } else {
-    alert("Sorry, try again.")
+    alert("Sorry, try again.");
 }
 if (cardTwo === cardOne) {
 
     alert("You found a match!");
 
 } else {
-    alert("Sorry, try again.")
+    alert("Sorry, try again.");
 }
 
 if (cardThree === cardFour) {
@@ -24,5 +24,18 @@ if (cardThree === cardFour) {
     alert("You found a match!");
 
 } else {
-    alert("Sorry, try again.")
+    alert("Sorry, try again.");
 }
+*/
+var board = document.getElementById("game-board");
+
+var createCards = function() {
+    for(var i = 0; i < 4; i++) {
+    var card = document.createElement("div");
+    card.className = "card";
+    board.appendChild(card);
+}
+};
+
+
+createCards();
